9e5eca859d011522e54fc758b72fffb2d2cbe8a8
Nurselam <77880259+WNurselam@users.noreply.github.com>
2022-06-12 18:37:55 +0300

    create footer

--- src/components/Todos/Footer/index.js
+++ src/components/Todos/Footer/index.js
@@ -1,9 +1,25 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-function Footer({todos,setTodos,sethide}) {
+function Footer({ todos, setTodos, sethide }) {
   return (
-    <div></div>
-  )
+    <footer className="footer">
+      <span className="todo-count">
+        <strong mv-value="todoLeft">0</strong>
+      </span>
+      <ul className="filters">
+        <li>
+          <a className="">All</a>
+        </li>
+        <li>
+          <a className="">Active</a>
+        </li>
+        <li>
+          <a className="">Completed</a>
+        </li>
+      </ul>
+      <button className="clear-completed">Clear completed</button>
+    </footer>
+  );
 }
 
 export default Footer;
